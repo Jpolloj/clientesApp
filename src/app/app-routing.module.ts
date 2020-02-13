fae544a404844d467cac2527aba72b2af11d2f81
jhon117 <jjcm_03@hotmail.com>
2020-02-13 00:29:27 -0500

    Prueba Asesoftware Terminada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListarComponent } from './listar/listar.component';
+import { DetailsComponent } from './details/details.component';
+import { EditComponent } from './edit/edit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'listar', component:ListarComponent},
+  {path:'details', component:DetailsComponent},
+  {path:'edit', component:EditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './listar/listar.component';
+
+import { FormsModule} from '@angular/forms'
+import { ServiceService} from '../app/Service/service.service'
+import { HttpClientModule } from '@angular/common/http';
+import { DetailsComponent } from './details/details.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    DetailsComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
